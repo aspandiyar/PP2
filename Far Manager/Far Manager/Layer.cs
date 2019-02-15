2898f45acbb42e44344f35f516f77eef02cf67cb
aspandiyar <32650178+aspandiyar@users.noreply.github.com>
2019-02-15 23:44:56 +0600

    Add files via upload

---
+++ Far Manager/Far Manager/Layer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Far_Manager
+{
+    /// <summary>
+    /// Класс для хранения информации о текущей директории, и возможных действиях с ними
+    /// </summary>
+    class Layer
+    {
+        //номер текущего элемента
+        private int selectedItem;
+        public int SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+
+            set
+            {
+                //зацикливание номера выбранного файла
+                if (value >= Directories.Count + Files.Count)
+                {
+                    selectedItem = 0;
+                } else if (value < 0)
+                {
+                    selectedItem = Directories.Count + Files.Count - 1;
+                } else
+                {
+                    selectedItem = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Лист со всеми папками
+        /// </summary>
+        public List<DirectoryInfo> Directories
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Лист со всеми файлами
+        /// </summary>
+        public List<FileInfo> Files
+        {
+            get;
+            set;
+        }
+        /// <summary>
+        /// Метод для удаления выбранной папки или файла
+        /// </summary>
+        public void DeleteSelectedItem()
+        {
+            if (selectedItem < Directories.Count)
+            {
+                Directory.Delete(Directories[selectedItem].FullName, true);
+                Directories.RemoveAt(selectedItem);
+            } else
+            {
+                File.Delete(Files[selectedItem - Directories.Count].FullName);
+                Files.RemoveAt(selectedItem - Directories.Count);
+            }
+                        
+            selectedItem--;
+        }
+
+        /// <summary>
+        /// Метод для рисования, и обновления состояния
+        /// </summary>
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            int page = selectedItem / (Console.WindowHeight - 1);
+            int i = (selectedItem + page) / (Console.WindowHeight - 1) * (Console.WindowHeight - 1);
+            for (; i < Math.Min(Directories.Count, (selectedItem + page) / (Console.WindowHeight - 1) * (Console.WindowHeight - 1) + Console.WindowHeight - 1); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine(i + 1 + ". " + Directories[i].Name);
+            }
+
+            i = Math.Max(Directories.Count, selectedItem / (Console.WindowHeight - 1) * (Console.WindowHeight - 1));
+            for (; i < Math.Min(Files.Count + Directories.Count, selectedItem / (Console.WindowHeight - 1) * (Console.WindowHeight - 1) + Console.WindowHeight - 1); i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                Console.WriteLine(i + 1 + ". " + Files[i - Directories.Count].Name);
+            }
+        }
+    }
+}
---
+++ Far Manager/Far Manager/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace Far_Manager
+{
+    //список с возможными состояниями системы
+    enum FarMode
+    {
+        DIR,
+        FILE
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Title - заголовок консоли
+             * history - стэк вложенных папок
+             * mode - текущее состояние системы
+             * root - путь к начальной папке
+             */
+            Console.Title = "Far Manager";
+            Stack<Layer> history = new Stack<Layer>();
+            FarMode mode = FarMode.DIR;
+            DirectoryInfo root = new DirectoryInfo(@"D:\");
+            bool run = true;
+            //в history засовываем папку root.
+            history.Push(
+                new Layer
+                {
+                    Directories = root.GetDirectories().ToList(),
+                    Files = root.GetFiles().ToList(),
+                    SelectedItem = 0
+                });
+            //цикл программы
+            while (run)
+            {
+                //автоматически обновлять интерфейс после каждого нажатия клавиши
+                if (mode == FarMode.DIR)
+                {
+                    history.Peek().Draw();
+                }
+                //ввод клавиши с клавиатуры и дальнейшие действия с ними
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    //на delete удалить файл
+                    case ConsoleKey.Delete:
+                        history.Peek().DeleteSelectedItem();
+                        break;
+                    //на вверх листать папки наверх
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SelectedItem--;
+                        break;
+                    //на вниз - листать вниз
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SelectedItem++;
+                        break;
+                    //на backspace - вернуться в прошлую папку, или закрыть файл
+                    case ConsoleKey.Backspace:
+                        if (history.Count == 1)
+                        {
+                            break;
+                        }
+
+                        if (mode == FarMode.DIR)
+                        {
+                            history.Pop();
+                        }
+                        else
+                        {
+                            mode = FarMode.DIR;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                    //на enter - открыть папку или файл
+                    case ConsoleKey.Enter:
+                        int x = history.Peek().SelectedItem;
+
+                        if (x < history.Peek().Directories.Count)
+                        {
+                            DirectoryInfo fileSystemInfo = history.Peek().Directories[x];
+
+                            DirectoryInfo directoryInfo = fileSystemInfo as DirectoryInfo;
+                            history.Push(
+                                new Layer
+                                {
+                                    Directories = directoryInfo.GetDirectories().ToList(),
+                                    Files = directoryInfo.GetFiles().ToList(),
+                                    SelectedItem = 0
+                                });
+                        }
+                        else
+                        {
+                            mode = FarMode.FILE;
+                            FileInfo fileInfo = history.Peek().Files[x - history.Peek().Directories.Count];
+                            Console.BackgroundColor = ConsoleColor.White;
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.Black;
+                            using(StreamReader sr = new StreamReader(fileInfo.FullName))
+                            {
+                                Console.WriteLine(sr.ReadToEnd());
+                            }
+                        }
+                        break;
+                    //на escape завершить выполнение программы
+                    case ConsoleKey.Escape:
+                        run = false;
+                        break;
+                    //на R переименовать файл или папку
+                    case ConsoleKey.R:
+                        string parentpath;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        int a = history.Peek().SelectedItem;
+                        Console.Clear();
+                        Console.Write("Введите новое имя файла: ");
+                        string txt = Console.ReadLine();
+                        if (a < history.Peek().Directories.Count)
+                        {
+                            parentpath = history.Peek().Directories[a].Parent.FullName;
+                            if (txt != history.Peek().Directories[a].Name)
+                            {
+                                Directory.Move(history.Peek().Directories[a].FullName, Path.Combine(parentpath, txt));
+                                DirectoryInfo df = new DirectoryInfo(parentpath);
+                                history.Peek().Directories = df.EnumerateDirectories().ToList();
+                            }
+                        }
+                        else
+                        {
+                            parentpath = history.Peek().Files[a - history.Peek().Directories.Count].FullName.Remove(history.Peek().Files[a - history.Peek().Directories.Count].FullName.Length - history.Peek().Files[a - history.Peek().Directories.Count].Name.Length);
+                            if (txt != history.Peek().Files[a - history.Peek().Directories.Count].Name)
+                            {
+                                File.Copy(history.Peek().Files[a - history.Peek().Directories.Count].FullName, Path.Combine(parentpath, txt));
+                                File.Delete(history.Peek().Files[a - history.Peek().Directories.Count].FullName);
+                                DirectoryInfo df = new DirectoryInfo(parentpath);
+                                history.Peek().Files = df.EnumerateFiles().ToList();
+                            }
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
